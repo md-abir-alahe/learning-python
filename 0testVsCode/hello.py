0003eeb247e54355ee43e955096fc7324fa82b92
md-abir-alahe <mdabiralahe@gmail.com>
2023-10-16 13:10:27 +0600

    first commit

---
+++ 0testVsCode/hello.py
@@ -0,0 +1,4 @@
+msg = "Abir is learning Python"
+print(msg);
+# This is first comment 
+
---
+++ 1usingPythonasAcalculator/numbers/numbers.py
@@ -0,0 +1,17 @@
+print((50-5*6)/4) # division always returns a floating point number
+
+# to get float division (/) and to do floor division and get an integer result we can use (//) operator. 
+print("result of 17/3 : ", 17/3);
+print(17//3);
+print(17%3);
+
+# (**) to calculate power
+print(2**3);
+
+width = 20;
+height = 5*9;
+print(width*height);
+
+tax = 12.5/100;
+price = 100.50;
+price * tax
